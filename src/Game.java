a2a7e64e7b398d84d8b2e797f7f0bbe7cb1210fa
kasumi <www.clean.com@gmail.com>
2018-05-13 01:41:33 +0800

    start to identify guess is right or not

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,6 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
-import java.util.regex.*;
+
 
 import static javafx.scene.input.KeyCode.Z;
 
@@ -51,20 +50,26 @@ public class Game {
 
     // 用戶輸入猜測字母
         public void guess() {
-           // guessChar = String.valueOf(System.in.read());
+            // guessChar = String.valueOf(System.in.read());
             //prOut("字在第 "+ guessChar.indexOf(str1)+"個");
-           // prOut(guessChar);
+            // prOut(guessChar);
             //isGetOrNot(guessChar);
 //char x = s.charAt(8) :將S字串裡的第8個字元設為X 字符
             // jnd.indexOf('a'): 找出jnd字串中第1次a出現的位置
             //^[A-Za-z]+$
             //if()
+
             Scanner guessing = new Scanner(System.in);
-            //Pattern pattern = Pattern.compile("[a-z][A-z]");
-            //Matcher matcher =pattern.matcher(str1);
-                    guessChar = guessing.next();//.next()
-                    prOut("输入的数据为：" + guessChar);
-      }
+
+            if (guessing.hasNext("[A-Za-z]")) {
+                guessChar = guessing.next();
+                prOut("输入的数据为：" + guessChar);
+                isGetOrNot(guessChar);
+            }else {
+                prOut("請輸入 英文字母");
+                guess();
+            }
+        }
 
 
     //檢查猜中字符及位罝
@@ -85,22 +90,14 @@ public class Game {
         public static void main(String[] args)throws Exception{
            Game guessMovie = new Game();
            String exmeTitle = guessMovie.getTheTitle();
-            //guessMovie.guess(guessMovie.getTheTitle());
-            for(int i = 10; i>0 ; i--){
+           for(int i = 10 ; i > 0 ; i-- ){
                 System.out.print("REMAIN :"+i+" times!\n");
                 System.out.print("Guess the movie:"+exmeTitle+">>"+exmeTitle.replaceAll(".","_"));//提示訊息
                 System.out.print("Enter a char");
-
                 guessMovie.guess();
-
-                //guessMovie.isGetOrNot();
-
             }
-
         }
-
-
-    }
+}
 
 
 
